c2bfb0bbd12d7ff554d05d0b15757a63c6e66442
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-03-20 17:03:50 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.19-1-MANJARO #1 SMP PREEMPT_DYNAMIC Thu, 13 Mar 2025 14:07:27 +0000 x86_64 GNU/Linux  17:03:50 up  7:04,  1 user,  load average: 0.86, 0.81, 0.84

--- chisel/playground/src/pipeline/execute/fu/Mdu.scala
+++ chisel/playground/src/pipeline/execute/fu/Mdu.scala
@@ -14,77 +14,78 @@ class Mdu extends Module {
   })
 
   io.result := 0.U
-  val iszero = Mux(io.src_info.src2_data === 0.U, 1.U,0.U)
+  val iszero = Mux(io.src_info.src2_data === 0.U, 1.U, 0.U)
   switch(io.info.op) {
     // Multiplication Operations
-    is(MDUOpType.mul) { 
+    is(MDUOpType.mul) {
       io.result := io.src_info.src1_data * io.src_info.src2_data // 64-bit Multiply
     }
-    is(MDUOpType.mulh) { 
+    is(MDUOpType.mulh) {
       val product = (io.src_info.src1_data.asSInt * io.src_info.src2_data.asSInt).asSInt
       io.result := product(127, 64).asUInt // High 64 bits of signed multiplication
     }
-    is(MDUOpType.mulhsu) { 
+    is(MDUOpType.mulhsu) {
       val product = (io.src_info.src1_data.asSInt * io.src_info.src2_data).asSInt
       io.result := product(127, 64).asUInt // High 64 bits of signed-unsigned multiplication
     }
-    is(MDUOpType.mulhu) { 
+    is(MDUOpType.mulhu) {
       val product = io.src_info.src1_data * io.src_info.src2_data
       io.result := product(127, 64) // High 64 bits of unsigned multiplication
     }
 
     // Division and Remainder Operations
-    is(MDUOpType.div) { 
-      val result = Mux(iszero === 1.U,SignedExtend(1.U,XLEN).asSInt,(io.src_info.src1_data.asSInt / io.src_info.src2_data.asSInt)) // Signed Division
-      val overflow = io.src_info.src2_data.asSInt === -1.S && io.src_info.src1_data === SignedExtend(1.U,XLEN)
-      io.result := Mux(overflow === true.B,SignedExtend(1.U,XLEN).asSInt,result).asUInt
+    is(MDUOpType.div) {
+      val result   = Mux(iszero === 1.U, SignedExtend(1.U, XLEN).asSInt, (io.src_info.src1_data.asSInt / io.src_info.src2_data.asSInt)) // Signed Division
+      val overflow = io.src_info.src2_data.asSInt === -1.S && io.src_info.src1_data === SignedExtend(1.U, XLEN)
+      io.result := Mux(overflow === true.B, SignedExtend(1.U, XLEN).asSInt, result).asUInt
     }
-    is(MDUOpType.divu) { 
-      val result = Mux(iszero === 1.U,SignedExtend(1.U,XLEN),(io.src_info.src1_data / io.src_info.src2_data)) // Unsigned Division
+    is(MDUOpType.divu) {
+      val result = Mux(iszero === 1.U, SignedExtend(1.U, XLEN), (io.src_info.src1_data / io.src_info.src2_data)) // Unsigned Division
       io.result := result
     }
-    is(MDUOpType.rem) { 
-      io.result := Mux(iszero === 1.U,io.src_info.src1_data.asUInt,(io.src_info.src1_data.asSInt % io.src_info.src2_data.asSInt).asUInt) // Signed Remainder
+    is(MDUOpType.rem) {
+      io.result := Mux(iszero === 1.U, io.src_info.src1_data.asUInt, (io.src_info.src1_data.asSInt % io.src_info.src2_data.asSInt).asUInt) // Signed Remainder
     }
-    is(MDUOpType.remu) { 
-      io.result := Mux(iszero === 1.U,io.src_info.src2_data,io.src_info.src1_data % io.src_info.src2_data) // Unsigned Remainder
+    is(MDUOpType.remu) {
+      io.result := Mux(iszero === 1.U, io.src_info.src2_data, io.src_info.src1_data % io.src_info.src2_data) // Unsigned Remainder
     }
 
     // Word-Type Operations (32-bit, sign-extended to 64-bit)
     is(MDUOpType.mulw) {
-      val mulResult = (io.src_info.src1_data(31, 0) * io.src_info.src2_data(31, 0))(31,0).asUInt
+      val mulResult = (io.src_info.src1_data(31, 0) * io.src_info.src2_data(31, 0))(31, 0).asUInt
       io.result := SignedExtend(mulResult, XLEN) // Use SignedExtend to extend to XLEN
     }
     is(MDUOpType.divw) {
       /*
-      val divtmp = io.src_info.src1_data(31, 0).asSInt / io.src_info.src2_data(31, 0).asSInt
-      val divResult = Mux(iszero === 1.U,(-1).S,divtmp) // right 
-      // val divResult = Mux(iszero === 1.U,(-1).S,io.src_info.src1_data(31, 0).asSInt / io.src_info.src2_data(31, 0).asSInt) // wrong , why ??
+      val divtmp                = io.src_info.src1_data(31, 0).asSInt / io.src_info.src2_data(31, 0).asSInt
+      val divResult             = Mux(iszero === 1.U,(-1).S,divtmp) // right
+      // val divResult          = Mux(iszero === 1.U,(-1).S,io.src_info.src1_data(31, 0).asSInt / io.src_info.src2_data(31, 0).asSInt) // wrong , why ??
       // printf(p"divResult: ${Hexadecimal(divResult)}, divtmp :${Hexadecimal(divtmp)}")
-      val overflow = io.src_info.src2_data.asSInt === -1.S && io.src_info.src1_data === SignedExtend(1.U,32)
+      val overflow              = io.src_info.src2_data.asSInt === -1.S && io.src_info.src1_data === SignedExtend(1.U,32)
       printf(p"${divtmp.asSInt}\n") // comment this line will get a wrong answer ??
       io.result := Mux(overflow === 1.U,SignedExtend(1.U,XLEN),SignedExtend(divResult.asUInt,XLEN))
-      */
-      val divtmp = io.src_info.src1_data(31, 0).asSInt / io.src_info.src2_data(31, 0).asSInt
-      val divResult = Mux(iszero === 1.U, (-1).S, divtmp.asSInt)
-      val overflow = io.src_info.src2_data.asSInt === -1.S && io.src_info.src1_data === SignedExtend(1.U, 32)
+       */
+      val divtmp                = io.src_info.src1_data(31, 0).asSInt / io.src_info.src2_data(31, 0).asSInt
+      val divResult             = Mux(iszero === 1.U, (-1).S, divtmp.asSInt)
+      val overflow              = io.src_info.src2_data.asSInt === -1.S && io.src_info.src1_data === SignedExtend(1.U, 32)
       printf(p"${divtmp.asSInt}\n") // comment this line will get a wrong answer ??
       io.result := Mux(overflow === 1.U, SignedExtend(1.U, XLEN), SignedExtend(divResult.asUInt, XLEN))
 
     }
     is(MDUOpType.divuw) {
-      val divtmp = (io.src_info.src1_data(31, 0) / io.src_info.src2_data(31, 0)).asSInt
-      val divResult = Mux(iszero === 1.U,(-1).S,divtmp)
+      val divtmp    = (io.src_info.src1_data(31, 0) / io.src_info.src2_data(31, 0)).asSInt
+      val divResult = Mux(iszero === 1.U, (-1).S, divtmp)
       io.result := SignedExtend(divResult.asUInt, XLEN) // Use SignedExtend to extend to XLEN
     }
     is(MDUOpType.remw) {
       printf(p"src1: ${Hexadecimal(io.src_info.src1_data)}, src2 :${Hexadecimal(io.src_info.src2_data)}")
-      val divtmp = (io.src_info.src1_data(31, 0).asSInt % io.src_info.src2_data(31, 0).asSInt)
-      val remResult = Mux(iszero === 1.U,io.src_info.src1_data.asSInt,divtmp)
+      val divtmp    = (io.src_info.src1_data(31, 0).asSInt % io.src_info.src2_data(31, 0).asSInt)
+      val remResult = Mux(iszero === 1.U, io.src_info.src1_data.asSInt, divtmp)
+      printf(p"${divtmp.asSInt}\n") // comment this line will get a wrong answer ??
       io.result := SignedExtend(remResult.asUInt, XLEN) // Use SignedExtend to extend to XLEN
     }
     is(MDUOpType.remuw) {
-      val remResult = Mux(iszero === 1.U,io.src_info.src1_data,(io.src_info.src1_data(31, 0) % io.src_info.src2_data(31, 0)).asUInt)
+      val remResult = Mux(iszero === 1.U, io.src_info.src1_data, (io.src_info.src1_data(31, 0) % io.src_info.src2_data(31, 0)).asUInt)
       io.result := SignedExtend(remResult, XLEN) // Use SignedExtend to extend to XLEN
     }
   }
