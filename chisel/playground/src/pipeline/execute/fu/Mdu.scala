24d0877052d2fc5145a4838f7e40996e21222f14
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-03-20 13:10:48 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.19-1-MANJARO #1 SMP PREEMPT_DYNAMIC Thu, 13 Mar 2025 14:07:27 +0000 x86_64 GNU/Linux  13:10:48 up  3:11,  1 user,  load average: 0.51, 0.76, 0.81

--- chisel/playground/src/pipeline/execute/fu/Mdu.scala
+++ chisel/playground/src/pipeline/execute/fu/Mdu.scala
@@ -57,7 +57,7 @@ class Mdu extends Module {
     is(MDUOpType.divw) {
       val divResult = Mux(iszero === 1.U,(-1).S,(io.src_info.src1_data(31, 0).asSInt / io.src_info.src2_data(31, 0).asSInt))
       val overflow = io.src_info.src2_data.asSInt === -1.S && io.src_info.src1_data === SignedExtend(1.U,32)
-      printf(p"Binary: ${Binary(SignedExtend(1.U,XLEN))}\n")
+      printf(p"Binary: ${Binary(SignedExtend(1.U,XLEN))},overflow : ${Binary(overflow)}, divresult :${Decimal(divResult)}\n")
       io.result := Mux(overflow === true.B,SignedExtend(1.U,XLEN),SignedExtend(divResult.asUInt,XLEN))
     }
     is(MDUOpType.divuw) {
