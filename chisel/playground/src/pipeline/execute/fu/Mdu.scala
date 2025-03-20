96239d2c25da71ffcca04370cee062d5014b3ff3
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-03-20 16:23:34 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.19-1-MANJARO #1 SMP PREEMPT_DYNAMIC Thu, 13 Mar 2025 14:07:27 +0000 x86_64 GNU/Linux  16:23:34 up  6:24,  1 user,  load average: 1.59, 1.36, 1.56

--- chisel/playground/src/pipeline/execute/fu/Mdu.scala
+++ chisel/playground/src/pipeline/execute/fu/Mdu.scala
@@ -71,6 +71,7 @@ class Mdu extends Module {
       io.result := SignedExtend(divResult.asUInt, XLEN) // Use SignedExtend to extend to XLEN
     }
     is(MDUOpType.remw) {
+      printf(p"src1: ${Hexadecimal(io.src_info.src1_data)}, src2 :${Hexadecimal(io.src_info.src2_data)}")
       val divtmp = (io.src_info.src1_data(31, 0).asSInt % io.src_info.src2_data(31, 0).asSInt)
       val remResult = Mux(iszero === 1.U,io.src_info.src1_data.asSInt,divtmp)
       io.result := SignedExtend(remResult.asUInt, XLEN) // Use SignedExtend to extend to XLEN
