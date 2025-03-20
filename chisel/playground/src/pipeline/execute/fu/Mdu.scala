1b8b5de399a2b862f6326256207cef46db9dd4ff
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-03-20 20:21:03 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.19-1-MANJARO #1 SMP PREEMPT_DYNAMIC Thu, 13 Mar 2025 14:07:27 +0000 x86_64 GNU/Linux  20:21:03 up 10:22,  1 user,  load average: 1.18, 1.14, 1.20

--- chisel/playground/src/pipeline/execute/fu/Mdu.scala
+++ chisel/playground/src/pipeline/execute/fu/Mdu.scala
@@ -106,7 +106,7 @@ class Mdu extends Module {
       // Result handling with Mux
       val remResult = Mux(iszero === 1.U, src1, rem) // Select between src1 (if zero) or divtmp
       io.result := SignedExtend(remResult,XLEN) // Extend the result to XLEN
-      printf(p"-1: ${Binary((-(1<<63).S).asUInt)}, iszero: ${(iszero)}\n")
+      printf(p"-1: ${Binary((-1.S(64.W)).asUInt)}, iszero: ${(iszero)}\n")
       printf(p"src1: ${Hexadecimal(io.src_info.src1_data)}, src2: ${Hexadecimal(io.src_info.src2_data)}\n")
 
       // Debugging can still be added, but it's optional now
