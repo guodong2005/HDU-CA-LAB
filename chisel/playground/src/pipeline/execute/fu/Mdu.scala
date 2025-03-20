a5e8b94889ec87546e3d6f89fbf0c2cdceae8da8
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-03-20 19:16:44 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.19-1-MANJARO #1 SMP PREEMPT_DYNAMIC Thu, 13 Mar 2025 14:07:27 +0000 x86_64 GNU/Linux  19:16:44 up  9:17,  1 user,  load average: 0.89, 0.77, 0.76

--- chisel/playground/src/pipeline/execute/fu/Mdu.scala
+++ chisel/playground/src/pipeline/execute/fu/Mdu.scala
@@ -15,7 +15,6 @@ class Mdu extends Module {
 
   io.result := 0.U
   val iszero = Mux(io.src_info.src2_data === 0.U, 1.U, 0.U)
-  val neg1_32 = (-1).S(32.W)
   val neg1_64 = (-1).S(64.W)
   switch(io.info.op) {
     // Multiplication Operations
@@ -59,7 +58,7 @@ class Mdu extends Module {
     }
     is(MDUOpType.divw) {
       val divtmp                = io.src_info.src1_data(31, 0).asSInt / io.src_info.src2_data(31, 0).asSInt
-      val divResult             = Mux(iszero === 1.U, neg1_32, divtmp.asSInt)(31,0)
+      val divResult             = Mux(iszero === 1.U, neg1_64, divtmp.asSInt)(31,0)
       val overflow              = io.src_info.src2_data.asSInt === neg1_64.asSInt && io.src_info.src1_data === SignedExtend(1.U, 32)
       // printf(p"result : ${divResult.asSInt}\n") // comment this line will get a wrong answer ??
       // printf(p"result width : ${divResult.getWidth}\n") // comment this line will get a wrong answer ??
