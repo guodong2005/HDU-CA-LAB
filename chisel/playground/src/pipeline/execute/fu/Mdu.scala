746f31934f4bdd3ef373d08bf46917e638b4f001
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-03-20 17:20:44 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.19-1-MANJARO #1 SMP PREEMPT_DYNAMIC Thu, 13 Mar 2025 14:07:27 +0000 x86_64 GNU/Linux  17:20:44 up  7:21,  1 user,  load average: 1.18, 1.18, 1.11

--- chisel/playground/src/pipeline/execute/fu/Mdu.scala
+++ chisel/playground/src/pipeline/execute/fu/Mdu.scala
@@ -82,7 +82,7 @@ class Mdu extends Module {
 
       val divtmp    = (io.src_info.src1_data(31, 0).asSInt % io.src_info.src2_data(31, 0).asSInt)(31,0)
       val remResult = Mux(iszero === 1.U, io.src_info.src1_data.asSInt, divtmp.asSInt)(31,0)
-      printf(p"${divtmp.asSInt(31)},width: ${remResult.getWidth}\n") // comment this line will get a wrong answer ??
+      printf(p"${divtmp(31)},width: ${remResult.getWidth}\n") // comment this line will get a wrong answer ??
       io.result := SignedExtend(remResult.asUInt, XLEN) // Use SignedExtend to extend to XLEN
     }
     is(MDUOpType.remuw) {
