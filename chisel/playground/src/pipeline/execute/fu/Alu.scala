97cff68e706e13ade307f7e8d2c69f062bd55955
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-03-20 11:06:21 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.19-1-MANJARO #1 SMP PREEMPT_DYNAMIC Thu, 13 Mar 2025 14:07:27 +0000 x86_64 GNU/Linux  11:06:21 up  1:07,  1 user,  load average: 1.56, 1.30, 1.09

--- chisel/playground/src/pipeline/execute/fu/Alu.scala
+++ chisel/playground/src/pipeline/execute/fu/Alu.scala
@@ -41,12 +41,11 @@ class Alu extends Module {
 
   is(ALUOpType.sllw) { 
     val shiftResult = (io.src_info.src1_data(31, 0) << io.src_info.src2_data(4, 0)).asUInt // 32-bit so it is 4-0
-    io.result := Cat(Fill(32, shiftResult(31)), shiftResult(31, 0)) // Sign-extend to 64 bits
+    io.result := SignedExtend(shiftResult,XLEN) // Sign-extend result to 64 bits
   }
 
   is(ALUOpType.srlw) { 
     val logicalShiftResult = (io.src_info.src1_data(31, 0) >> io.src_info.src2_data(4, 0)).asUInt // same as above
-    io.result := Cat(Fill(32, logicalShiftResult(31)), logicalShiftResult(31, 0)) // Zero-extend to 64 bits
     io.result := SignedExtend(logicalShiftResult,XLEN) // Sign-extend result to 64 bits
   }
 
