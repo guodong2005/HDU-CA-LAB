fd4666911d1c22189363d24b60857a5ae47f1cd1
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-03-20 16:16:06 +0800

    > test lab3 23150919 WangJiadong Linux guodong-manjaro 6.12.19-1-MANJARO #1 SMP PREEMPT_DYNAMIC Thu, 13 Mar 2025 14:07:27 +0000 x86_64 GNU/Linux  16:16:06 up  6:17,  1 user,  load average: 2.04, 1.81, 1.76

--- difftest/core/PuaCpu.v
+++ difftest/core/PuaCpu.v
@@ -930,7 +930,8 @@ module Mdu(	// home/guodong/hdu-2025-ca-lab/chisel/playground/src/pipeline/execu
   wire         _GEN_9 = io_info_op == 5'hC;	// home/guodong/hdu-2025-ca-lab/chisel/playground/src/pipeline/execute/fu/Mdu.scala:18:22
   wire [32:0]  _GEN_10 = {io_src_info_src1_data[31], io_src_info_src1_data[31:0]};	// home/guodong/hdu-2025-ca-lab/chisel/playground/src/pipeline/execute/fu/Mdu.scala:59:{57,177}
   wire [32:0]  _GEN_11 = {io_src_info_src2_data[31], io_src_info_src2_data[31:0]};	// home/guodong/hdu-2025-ca-lab/chisel/playground/src/pipeline/execute/fu/Mdu.scala:59:{115,177}
-  wire [32:0]  divResult = iszero ? 33'h1FFFFFFFF : $signed(_GEN_10) / $signed(_GEN_11);	// home/guodong/hdu-2025-ca-lab/chisel/playground/src/pipeline/execute/fu/Mdu.scala:17:49, :59:177, :61:{26,86}
+  wire [32:0]  divtmp = $signed(_GEN_10) / $signed(_GEN_11);	// home/guodong/hdu-2025-ca-lab/chisel/playground/src/pipeline/execute/fu/Mdu.scala:59:177, :60:56
+  wire [32:0]  divResult = iszero ? 33'h1FFFFFFFF : divtmp;	// home/guodong/hdu-2025-ca-lab/chisel/playground/src/pipeline/execute/fu/Mdu.scala:17:49, :60:56, :61:26
   `ifndef SYNTHESIS	// home/guodong/hdu-2025-ca-lab/chisel/playground/src/pipeline/execute/fu/Mdu.scala:59:13
     always @(posedge clock) begin	// home/guodong/hdu-2025-ca-lab/chisel/playground/src/pipeline/execute/fu/Mdu.scala:59:13
       if ((`PRINTF_COND_) & (|io_info_op) & ~_GEN_0 & ~_GEN_2 & ~_GEN_3 & ~_GEN_4
@@ -939,7 +940,7 @@ module Mdu(	// home/guodong/hdu-2025-ca-lab/chisel/playground/src/pipeline/execu
                 io_src_info_src1_data[31:0], io_src_info_src2_data[31:0],
                 $signed(_GEN_10) / $signed(_GEN_11));	// home/guodong/hdu-2025-ca-lab/chisel/playground/src/pipeline/execute/fu/Mdu.scala:59:{13,57,115,177}
         $fwrite(32'h80000002, "iszero === 1. U %d, and iszero : %d\n", iszero, 1'h1);	// home/guodong/hdu-2025-ca-lab/chisel/playground/src/pipeline/execute/fu/Mdu.scala:17:49, :39:51, :59:13, :63:13
-        $fwrite(32'h80000002, "%d\n", $signed(_GEN_10) / $signed(_GEN_11));	// home/guodong/hdu-2025-ca-lab/chisel/playground/src/pipeline/execute/fu/Mdu.scala:59:{13,177}, :60:56, :64:13
+        $fwrite(32'h80000002, "%d\n", divtmp);	// home/guodong/hdu-2025-ca-lab/chisel/playground/src/pipeline/execute/fu/Mdu.scala:59:13, :60:56, :64:13
       end
     end // always @(posedge)
   `endif // not def SYNTHESIS
