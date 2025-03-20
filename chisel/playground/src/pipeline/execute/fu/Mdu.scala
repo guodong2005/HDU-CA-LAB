d3c5aac9416d2cc1ef7beab6230a8a7c47887312
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-03-20 19:26:37 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.19-1-MANJARO #1 SMP PREEMPT_DYNAMIC Thu, 13 Mar 2025 14:07:27 +0000 x86_64 GNU/Linux  19:26:37 up  9:27,  1 user,  load average: 3.05, 1.73, 1.16

--- chisel/playground/src/pipeline/execute/fu/Mdu.scala
+++ chisel/playground/src/pipeline/execute/fu/Mdu.scala
@@ -72,13 +72,13 @@ class Mdu extends Module {
 
       val divtmp    = (io.src_info.src1_data(31, 0).asSInt % io.src_info.src2_data(31, 0).asSInt)(31,0)
       val remResult = Mux(iszero === 1.U, io.src_info.src1_data.asSInt, divtmp.asSInt)
-     printf(p"res width: ${remResult.getWidth},sign bit : ${remResult(31)}\n") // comment this line will get a wrong answer ??
+    //  printf(p"res width: ${remResult.getWidth},sign bit : ${remResult(31)}\n") // comment this line will get a wrong answer ??
       io.result := SignedExtend(remResult.asUInt, XLEN) // Use SignedExtend to extend to XLEN
     }
     is(MDUOpType.remuw) {
       val divtmp    = (io.src_info.src1_data(31, 0).asUInt % io.src_info.src2_data(31, 0).asUInt)(31,0)
       val remResult = Mux(iszero === 1.U, io.src_info.src1_data, divtmp)
-      // printf(p"${divtmp(31)},width: ${remResult.getWidth}\n") // comment this line will get a wrong answer ??
+      printf(p"${divtmp(31)},width: ${remResult.getWidth}\n") // comment this line will get a wrong answer ??
       io.result := SignedExtend(remResult, XLEN) // Use SignedExtend to extend to XLEN
     }
   }
