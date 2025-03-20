9e774e35de23bfb97f11565ac425abb52e83e6cd
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-03-20 13:27:13 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.19-1-MANJARO #1 SMP PREEMPT_DYNAMIC Thu, 13 Mar 2025 14:07:27 +0000 x86_64 GNU/Linux  13:27:13 up  3:28,  1 user,  load average: 1.21, 1.68, 1.61

--- chisel/playground/src/pipeline/execute/fu/Mdu.scala
+++ chisel/playground/src/pipeline/execute/fu/Mdu.scala
@@ -55,7 +55,7 @@ class Mdu extends Module {
       io.result := SignedExtend(mulResult, XLEN) // Use SignedExtend to extend to XLEN
     }
     is(MDUOpType.divw) {
-      printf(p"src1: ${Decimal(io.src_info.src1_data(31,0).asSInt)}, src2 :${Decimal(io.src_info.src2_data(31,0).asSInt)}\n")
+      printf(p"src1: ${Decimal(io.src_info.src1_data(31,0).asSInt)}, src2 :${Decimal(io.src_info.src2_data(31,0).asUInt)}\n")
       val divResult = Mux(iszero === 1.U,(-1).S,(io.src_info.src1_data(31, 0).asSInt / io.src_info.src2_data(31, 0).asSInt))
       val overflow = io.src_info.src2_data.asSInt === -1.S && io.src_info.src1_data === SignedExtend(1.U,32)
       io.result := Mux(overflow === true.B,SignedExtend(1.U,XLEN),SignedExtend(divResult.asUInt,XLEN))
