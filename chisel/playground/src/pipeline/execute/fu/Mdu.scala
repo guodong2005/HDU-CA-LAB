6d581fc22724b3ff92845218e82c5118547c64e1
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-03-20 19:44:36 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.19-1-MANJARO #1 SMP PREEMPT_DYNAMIC Thu, 13 Mar 2025 14:07:27 +0000 x86_64 GNU/Linux  19:44:36 up  9:45,  1 user,  load average: 1.11, 1.28, 1.25

--- chisel/playground/src/pipeline/execute/fu/Mdu.scala
+++ chisel/playground/src/pipeline/execute/fu/Mdu.scala
@@ -73,8 +73,8 @@ class Mdu extends Module {
       val divtmp    = (io.src_info.src1_data(31, 0).asSInt % io.src_info.src2_data(31, 0).asSInt)(31,0)
       val remResult = Mux(iszero === 1.U, io.src_info.src1_data.asUInt, SignedExtend(divtmp.asUInt,XLEN))
     //  printf(p"res width: ${remResult.getWidth},sign bit : ${remResult(31)}\n") // comment this line will get a wrong answer ??
-      io.result := SignedExtend(remResult.asUInt, XLEN) // Use SignedExtend to extend to XLEN
-  printf(p"divtmp: ${(io.result(31))}, width: ${Hexadecimal(io.result)},iszero : ${iszero}\n")
+      io.result := remResult.asUInt // Use SignedExtend to extend to XLEN
+  printf(p"divtmp: ${(remResult.getWidth)}, width: ${Hexadecimal(io.result)},iszero : ${iszero}\n")
     }
    is(MDUOpType.remuw) {
   // Extracting the relevant bits for the remainder operation
