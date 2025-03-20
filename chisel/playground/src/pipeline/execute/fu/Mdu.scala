b8f284b2ddce767c9cc8b0af4a2c6096c5620e53
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-03-20 16:24:45 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.19-1-MANJARO #1 SMP PREEMPT_DYNAMIC Thu, 13 Mar 2025 14:07:27 +0000 x86_64 GNU/Linux  16:24:45 up  6:25,  1 user,  load average: 2.39, 1.62, 1.63

--- chisel/playground/src/pipeline/execute/fu/Mdu.scala
+++ chisel/playground/src/pipeline/execute/fu/Mdu.scala
@@ -59,10 +59,10 @@ class Mdu extends Module {
       val divtmp = io.src_info.src1_data(31, 0).asSInt / io.src_info.src2_data(31, 0).asSInt
       val divResult = Mux(iszero === 1.U,(-1).S,divtmp) // right 
       // val divResult = Mux(iszero === 1.U,(-1).S,io.src_info.src1_data(31, 0).asSInt / io.src_info.src2_data(31, 0).asSInt) // wrong , why ??
-      printf(p"divResult: ${Hexadecimal(divResult)}, divtmp :${Hexadecimal(divtmp)}")
+      // printf(p"divResult: ${Hexadecimal(divResult)}, divtmp :${Hexadecimal(divtmp)}")
       val overflow = io.src_info.src2_data.asSInt === -1.S && io.src_info.src1_data === SignedExtend(1.U,32)
-      printf(p"iszero === 1. U ${iszero === 1.U}, and iszero : ${(-1).S}\n")
-      printf(p"${divtmp.asSInt}\n")
+      // printf(p"iszero === 1. U ${iszero === 1.U}, and iszero : ${(-1).S}\n")
+      // printf(p"${divtmp.asSInt}\n")
       io.result := Mux(overflow === 1.U,SignedExtend(1.U,XLEN),SignedExtend(divResult.asUInt,XLEN))
     }
     is(MDUOpType.divuw) {
