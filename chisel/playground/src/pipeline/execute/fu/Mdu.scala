ffe8fec43da795264e7cb0e0e84eba1f54080034
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-03-20 19:56:18 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.19-1-MANJARO #1 SMP PREEMPT_DYNAMIC Thu, 13 Mar 2025 14:07:27 +0000 x86_64 GNU/Linux  19:56:18 up  9:57,  1 user,  load average: 1.05, 1.32, 1.36

--- chisel/playground/src/pipeline/execute/fu/Mdu.scala
+++ chisel/playground/src/pipeline/execute/fu/Mdu.scala
@@ -51,7 +51,7 @@ class Mdu extends Module {
       val quotient = src1 / src2
       val rem      = src1 - quotient * src2
 
-      io.result := Mux(iszero === 1.U, io.src_info.src1_data.asSInt, rem) // Signed Remainder
+      io.result := Mux(iszero === 1.U, io.src_info.src1_data.asSInt, rem.asSInt) // Signed Remainder
     }
     is(MDUOpType.remu) {
       val src1 = io.src_info.src1_data.asUInt // 32-bit source 1
