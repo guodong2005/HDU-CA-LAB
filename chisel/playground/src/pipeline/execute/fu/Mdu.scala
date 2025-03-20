b4864031fc58f9630cfd1dfb52c990e99bc56510
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-03-20 17:18:56 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.19-1-MANJARO #1 SMP PREEMPT_DYNAMIC Thu, 13 Mar 2025 14:07:27 +0000 x86_64 GNU/Linux  17:18:56 up  7:20,  1 user,  load average: 1.31, 1.25, 1.12

--- chisel/playground/src/pipeline/execute/fu/Mdu.scala
+++ chisel/playground/src/pipeline/execute/fu/Mdu.scala
@@ -82,7 +82,7 @@ class Mdu extends Module {
       printf(p"iszero: ${Hexadecimal(iszero)}, src2 :${Hexadecimal(iszero === 1.U)}\n")
 
       val divtmp    = (io.src_info.src1_data(31, 0).asSInt % io.src_info.src2_data(31, 0).asSInt)(31,0)
-      val remResult = Mux(iszero === 1.U, io.src_info.src1_data.asSInt, divtmp.asSInt)
+      val remResult = Mux(iszero === 1.U, io.src_info.src1_data.asSInt, divtmp.asSInt)(31,0)
       printf(p"${remResult.asSInt(31)},width: ${remResult.getWidth}\n") // comment this line will get a wrong answer ??
       io.result := SignedExtend(remResult.asUInt, XLEN) // Use SignedExtend to extend to XLEN
     }
