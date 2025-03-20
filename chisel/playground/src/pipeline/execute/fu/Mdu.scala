2d4c943611f9f001834de5530b5075f15569ee0a
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-03-20 17:16:57 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.19-1-MANJARO #1 SMP PREEMPT_DYNAMIC Thu, 13 Mar 2025 14:07:27 +0000 x86_64 GNU/Linux  17:16:57 up  7:18,  1 user,  load average: 0.42, 1.10, 1.07

--- chisel/playground/src/pipeline/execute/fu/Mdu.scala
+++ chisel/playground/src/pipeline/execute/fu/Mdu.scala
@@ -81,11 +81,10 @@ class Mdu extends Module {
       printf(p"src1: ${Hexadecimal(io.src_info.src1_data)}, src2 :${Hexadecimal(io.src_info.src2_data)}")
       printf(p"iszero: ${Hexadecimal(iszero)}, src2 :${Hexadecimal(iszero === 1.U)}\n")
 
-      val divtmp    = (io.src_info.src1_data(31, 0).asSInt % io.src_info.src2_data(31, 0).asSInt)
+      val divtmp    = (io.src_info.src1_data(31, 0).asSInt % io.src_info.src2_data(31, 0).asSInt)(31,0)
       val remResult = Mux(iszero === 1.U, io.src_info.src1_data.asSInt, divtmp)
       printf(p"${remResult.asSInt(31)}\n") // comment this line will get a wrong answer ??
-      io.result := Cat(Fill(32,remResult(31)),remResult(31,0))
-      // io.result := SignedExtend(remResult.asUInt, XLEN) // Use SignedExtend to extend to XLEN
+      io.result := SignedExtend(remResult.asUInt, XLEN) // Use SignedExtend to extend to XLEN
     }
     is(MDUOpType.remuw) {
       val remResult = Mux(iszero === 1.U, io.src_info.src1_data, (io.src_info.src1_data(31, 0) % io.src_info.src2_data(31, 0)).asUInt)
