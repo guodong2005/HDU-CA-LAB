73c42a8975dfa9d17b85ee3cb294bdc3c6d97d76
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-03-22 23:18:08 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.19-1-MANJARO #1 SMP PREEMPT_DYNAMIC Thu, 13 Mar 2025 14:07:27 +0000 x86_64 GNU/Linux  23:18:08 up  3:35,  1 user,  load average: 1.98, 1.09, 0.83

--- chisel/playground/src/pipeline/memory/MemoryUnit.scala
+++ chisel/playground/src/pipeline/memory/MemoryUnit.scala
@@ -11,23 +11,38 @@ class MemoryUnit extends Module {
   val io = IO(new Bundle {
     val memoryStage    = Input(new ExecuteUnitMemoryUnit())
     val writeBackStage = Output(new MemoryUnitWriteBackUnit())
-    val dataSram     = new DataSram()
+    val dataSram       = new DataSram()
   })
 
   val info = io.memoryStage.data.info
-  io.writeBackStage.data.pc                        := io.memoryStage.data.pc
-  io.writeBackStage.data.info                      := io.memoryStage.data.info
+  io.writeBackStage.data.pc   := io.memoryStage.data.pc
+  io.writeBackStage.data.info := io.memoryStage.data.info
   // val memAddr = dataSram.addr  I guess this wont work
-  val memAddr = io.memoryStage.data.rd_info.addr3.asUInt * 7.U
-  val memData = Mux(
+  val memAddr = io.memoryStage.data.rd_info.addr3.asUInt * 8.U
+  val memData = Wire(UInt(8.W)) // Define memData as a 8-bit wide wire for flexibility
+
+// Generate LookUpTree to assign memData based on memAddr
+  switch(memAddr) {
+    is(0.U) { memData := io.dataSram.rdata(7, 0).asUInt }
+    is(8.U) { memData := io.dataSram.rdata(15, 8).asUInt }
+    is(16.U) { memData := io.dataSram.rdata(23, 16).asUInt }
+    is(24.U) { memData := io.dataSram.rdata(31, 24).asUInt }
+    is(32.U) { memData := io.dataSram.rdata(39, 32).asUInt }
+    is(40.U) { memData := io.dataSram.rdata(47, 40).asUInt }
+    is(48.U) { memData := io.dataSram.rdata(55, 48).asUInt }
+    is(56.U) { memData := io.dataSram.rdata(63, 56).asUInt }
+  }
+
+// Use memData in your Mux logic
+  val finalMemData = Mux(
     info.fusel === FuType.lsu,
-    io.dataSram.rdata(63, memAddr.asUInt.litValue.toInt).asUInt,
+    memData,
     0.U
   )
   val extendedData = Mux(
     info.op(2) === 1.U,
-    ZeroExtend(memData,XLEN),
-    SignedExtend(memData,XLEN)
+    ZeroExtend(finalMemData, XLEN),
+    SignedExtend(finalMemData, XLEN)
   )
-  io.writeBackStage.data.rd_info.wdata             := Mux(info.fusel === FuType.lsu,extendedData,io.memoryStage.data.rd_info.wdata)
+  io.writeBackStage.data.rd_info.wdata := Mux(info.fusel === FuType.lsu, extendedData, io.memoryStage.data.rd_info.wdata)
 }
