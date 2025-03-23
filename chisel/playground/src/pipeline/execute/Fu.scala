7d471befdda8b6b825d962ecdd742f849019869c
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-03-23 12:33:35 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.19-1-MANJARO #1 SMP PREEMPT_DYNAMIC Thu, 13 Mar 2025 14:07:27 +0000 x86_64 GNU/Linux  12:33:35 up  2:28,  1 user,  load average: 0.76, 0.97, 0.87

--- chisel/playground/src/pipeline/execute/Fu.scala
+++ chisel/playground/src/pipeline/execute/Fu.scala
@@ -33,9 +33,9 @@ class Fu extends Module with HasInstrType{
   mdu.io.info := io.data.info
   mdu.io.src_info := io.data.src_info
 
-  lsu.io.dataSram <> io.dataSram // same as := ? not, but I should have a deeper understanding !
   lsu.io.info := io.data.info
   lsu.io.src_info := io.data.src_info
+  lsu.io.dataSram <> io.dataSram // same as := ? not, but I should have a deeper understanding !
 
   val result = LookupTree(
     io.data.info.fusel,
