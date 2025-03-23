c8a7899e519071e327c8231f38bac3549d797b6f
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-03-23 21:03:16 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.19-1-MANJARO #1 SMP PREEMPT_DYNAMIC Thu, 13 Mar 2025 14:07:27 +0000 x86_64 GNU/Linux  21:03:16 up 10:58,  1 user,  load average: 1.50, 1.30, 1.12

--- chisel/playground/src/pipeline/decode/Decoder.scala
+++ chisel/playground/src/pipeline/decode/Decoder.scala
@@ -67,7 +67,7 @@ class Decoder extends Module with HasInstrType {
     setInfo(inst, rd, 0.U, 0.U, ALUOpType.add, true.B, false.B, false.B, true.B)
   }.elsewhen(instrType === InstrS){
     //                     src2     writeback src1en src2en   valid
-    setInfo(inst, rd, rs1, rs2, op, false.B, true.B, true.B, true.B)
+    setInfo(inst, rd, rs1, rs2, op, false.B, true.B, false.B, true.B)
   }.elsewhen(instrType === InstrB){
     //                     src2     writeback src1en src2en   valid
     setInfo(inst, 0.U, rs1, rs2, op, false.B, true.B, true.B, true.B)
