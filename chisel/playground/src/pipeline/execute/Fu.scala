c0f50c61e520c50e06eeeab255e30a4d05ae86af
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-03-23 16:21:24 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.19-1-MANJARO #1 SMP PREEMPT_DYNAMIC Thu, 13 Mar 2025 14:07:27 +0000 x86_64 GNU/Linux  16:21:24 up  6:16,  1 user,  load average: 0.94, 0.63, 0.60

--- chisel/playground/src/pipeline/execute/Fu.scala
+++ chisel/playground/src/pipeline/execute/Fu.scala
@@ -47,7 +47,7 @@ class Fu extends Module with HasInstrType{
       // FuType.lsu -> lsu.io.result
     )
   ) 
-  io.data.rd_info.wdata := Mux(io.data.info.fusel === FuType.alu,alu.io.result,mdu.io.result) // lsu doesnt matter
+  io.data.rd_info.wdata := result // lsu doesnt matter
   // io.data.rd_info.addr3 := Mux(io.data.info.fusel === FuType.lsu,lsu.io.addr3,0.U)
    io.data.rd_info.addr3 := lsu.io.addr3
 }
--- chisel/playground/src/pipeline/execute/fu/Lsu.scala
+++ chisel/playground/src/pipeline/execute/fu/Lsu.scala
@@ -30,7 +30,7 @@ in some case like the datasram.addr[0] = 1 and the command type is double word m
 
 TODO: add unaligned exception
    */
-  val tmpwen = io.info.op(1, 0) << io.dataSram.addr(2, 0)
+  val tmpwen = ZeroExtend(((io.info.op(1, 0).asUInt) << (io.dataSram.addr(2, 0).asUInt)),8)
   // io.dataSram.wen := tmpwen &&
   io.dataSram.en   := !reset.asBool
   io.dataSram.wen  := tmpwen & Fill(8, io.info.valid && (io.info.fusel === FuType.lsu) && LSUOpType.isStore(io.info.op))
