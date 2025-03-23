79a5f9834b15d1d5dcbf53b7fd237d97854c57fb
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-03-23 17:00:18 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.19-1-MANJARO #1 SMP PREEMPT_DYNAMIC Thu, 13 Mar 2025 14:07:27 +0000 x86_64 GNU/Linux  17:00:18 up  6:55,  1 user,  load average: 1.74, 1.25, 0.92

--- chisel/playground/src/pipeline/execute/fu/Lsu.scala
+++ chisel/playground/src/pipeline/execute/fu/Lsu.scala
@@ -31,7 +31,9 @@ in some case like the datasram.addr[0] = 1 and the command type is double word m
 TODO: add unaligned exception
    */
   io.dataSram.addr := io.src_info.src1_data + io.info.imm
-  val bits = Fill(PopCount(io.info.op(1, 0)).litValue.toInt, 1.U)
+  val count = PopCount(io.info.op(1, 0))
+  val bits = (1.U << count) - 1.U // Create a dynamic number of bits (all 1s)
+
   val tmpwen = ZeroExtend((bits << (io.dataSram.addr(2, 0).asUInt)),8)
   // printf("tmpwen : ${tmpwen}.io.info.op,io.src_info.src2_data\n")
   printf(p"tmpwen: ${Binary(ZeroExtend(tmpwen,8))}, io.info.op: ${io.info.op}, io.src_info.src2_data: ${Hexadecimal(io.src_info.src2_data)}\n")
