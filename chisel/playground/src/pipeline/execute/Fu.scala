19e84e8e04bcb05a5dbfe6cd8ed2c040aaeb0550
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-03-23 13:05:30 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.19-1-MANJARO #1 SMP PREEMPT_DYNAMIC Thu, 13 Mar 2025 14:07:27 +0000 x86_64 GNU/Linux  13:05:30 up  3:00,  1 user,  load average: 0.75, 0.66, 0.74

--- chisel/playground/src/pipeline/execute/Fu.scala
+++ chisel/playground/src/pipeline/execute/Fu.scala
@@ -47,4 +47,5 @@ class Fu extends Module with HasInstrType{
   ) 
   io.data.rd_info.wdata := Mux(io.data.info.fusel === FuType.alu,alu.io.result,mdu.io.result) // lsu doesnt matter
   // io.data.rd_info.addr3 := Mux(io.data.info.fusel === FuType.lsu,lsu.io.addr3,0.U)
+   io.data.rd_info.addr3 := 1.U 
 }
