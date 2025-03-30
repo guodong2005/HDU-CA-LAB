9ddf088c5118d77c939578cb40ffa93d642563f7
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-03-30 11:36:40 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.19-1-MANJARO #1 SMP PREEMPT_DYNAMIC Thu, 13 Mar 2025 14:07:27 +0000 x86_64 GNU/Linux  11:36:40 up  1:14,  1 user,  load average: 2.17, 1.89, 1.63

--- chisel/playground/src/pipeline/execute/fu/Bru.scala
+++ chisel/playground/src/pipeline/execute/fu/Bru.scala
@@ -29,7 +29,7 @@ class Bru extends Module {
   // Debugging with printf
 
   // Instruction handling
-  printf("bru triggered");
+  printf("bru triggered\n");
   switch(info.op) {
     // JAL (Jump and Link)
     is(BRUOpType.jal) {
