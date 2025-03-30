791319cd6457adcccff24f4684aa09be0b791db2
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-03-30 17:33:35 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.19-1-MANJARO #1 SMP PREEMPT_DYNAMIC Thu, 13 Mar 2025 14:07:27 +0000 x86_64 GNU/Linux  17:33:35 up  1:36,  1 user,  load average: 1.09, 1.10, 0.98

--- chisel/playground/src/defines/Bundles.scala
+++ chisel/playground/src/defines/Bundles.scala
@@ -34,6 +34,7 @@ class Info extends Bundle {
   val src1_ren   = Bool()
   val src2_ren   = Bool()
   val fusel      = UInt(2.W)
+  val pc         = UInt(XLEN.W) // Add this line for the missing pc field
 }
 object Info {
   def default: Info = {
@@ -49,6 +50,7 @@ object Info {
     info.src1_ren   := true.B
     info.src2_ren   := false.B
     info.fusel      := 0.U
+    info.pc         := 0.U // Add this line to initialize the pc field
     info
   }
 }
