3bf1f1d58cf75a42d311b13f13d208b6d05927d3
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-05-01 23:33:10 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.20-2-MANJARO #1 SMP PREEMPT_DYNAMIC Mon, 24 Mar 2025 06:33:36 +0000 x86_64 GNU/Linux  23:33:10 up  1:15,  1 user,  load average: 3.78, 4.11, 3.54

--- chisel/playground/src/pipeline/decode/DecodeUnit.scala
+++ chisel/playground/src/pipeline/decode/DecodeUnit.scala
@@ -20,13 +20,13 @@ class DecodeUnit extends Module with HasInstrType {
   val pc     = io.decodeStage.data.pc
   val info   = Wire(new Info())
   val is_lui = decoder.io.out.info.instr(31, 25) === "b0001110".U // is pccadd12ui
-  printf(p"decodeUnit ${Hexadecimal(inst)}\n");
   printf(p"islui ${Hexadecimal(is_lui)}\n");
 
   val inst = decoder.io.out.info.instr
   val instrType :: fuType :: fuOpType :: Nil =
     ListLookup(inst, Instructions.DecodeDefault, Instructions.DecodeTable)
 
+  printf(p"decodeUnit ${Hexadecimal(inst)}\n");
   val imm = LookupTree(
     instrType,
     Seq(
