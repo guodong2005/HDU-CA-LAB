68b08bc4d286650f39dcad64f74dc12671afe86c
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-05-04 20:37:05 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.20-2-MANJARO #1 SMP PREEMPT_DYNAMIC Mon, 24 Mar 2025 06:33:36 +0000 x86_64 GNU/Linux  20:37:05 up  6:01,  1 user,  load average: 3.14, 2.61, 2.65

--- chisel/playground/src/PuaCpu.scala
+++ chisel/playground/src/PuaCpu.scala
@@ -12,6 +12,7 @@ class PuaCpu extends Module {
 
   val core = Module(new Core())
 
+  dontTouch(io.axi)
   io.ext_int <> core.io.interrupt
   io.axi     <> core.io.axi
   io.debug   <> core.io.debug
