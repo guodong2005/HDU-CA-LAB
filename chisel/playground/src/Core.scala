430dfe3329857aadc6bad1c52618119a05e36917
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-05-04 21:10:51 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.20-2-MANJARO #1 SMP PREEMPT_DYNAMIC Mon, 24 Mar 2025 06:33:36 +0000 x86_64 GNU/Linux  21:10:51 up  6:34,  1 user,  load average: 3.41, 2.85, 2.82

--- chisel/playground/src/Core.scala
+++ chisel/playground/src/Core.scala
@@ -11,7 +11,7 @@ import dataclass.data
 class Core extends Module {
   val io = IO(new Bundle {
     // val interrupt = Input(new ExtInterrupt())
-    val interrupt = UInt(8.W)
+    val interrupt = Input(UInt(8.W))
     val axi       = new AXI()
     val debug     = new DEBUG()
     val dataSram  = new DataSram()
