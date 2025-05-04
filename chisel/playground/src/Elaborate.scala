dd2892211da871ef007ecf02468aa38149eb1f20
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-05-04 20:45:00 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.20-2-MANJARO #1 SMP PREEMPT_DYNAMIC Mon, 24 Mar 2025 06:33:36 +0000 x86_64 GNU/Linux  20:45:00 up  6:09,  1 user,  load average: 4.94, 3.09, 2.78

--- chisel/playground/src/Elaborate.scala
+++ chisel/playground/src/Elaborate.scala
@@ -3,7 +3,7 @@ import circt.stage._
 
 object Elaborate extends App {
   implicit val cpuConfig = new CpuConfig()
-  def top                = new PuaCpu()
+  def top                = new core_top()
   val generator          = Seq(chisel3.stage.ChiselGeneratorAnnotation(() => top))
   (new ChiselStage).execute(args, generator :+ CIRCTTargetAnnotation(CIRCTTarget.Verilog))
 }
