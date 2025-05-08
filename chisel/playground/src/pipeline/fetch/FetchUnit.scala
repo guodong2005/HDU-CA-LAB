0104545bf2339a4d4ed8568e6b784d3ff7c0c483
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-05-08 08:49:55 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.20-2-MANJARO #1 SMP PREEMPT_DYNAMIC Mon, 24 Mar 2025 06:33:36 +0000 x86_64 GNU/Linux  08:49:55 up 43 min,  1 user,  load average: 3.99, 3.14, 2.58

--- chisel/playground/src/pipeline/fetch/FetchUnit.scala
+++ chisel/playground/src/pipeline/fetch/FetchUnit.scala
@@ -16,6 +16,8 @@ class FetchUnit extends Module {
     val branch       = Input(Bool())
     val target       = Input(UInt(XLEN.W))
     val signal       = Input(new Signals())
+    val valid        = Input(Bool())
+    val inst         = Input(UInt(XLEN.W))
     val fetchrequest = Output(new FetchRequest())
   })
 
@@ -41,8 +43,8 @@ class FetchUnit extends Module {
     isValid := Mux(io.signal.fetchUnitSignal.allow_to_go === true.B, true.B, false.B)
   }
 
-  io.fetchrequest.addr  = pc
-  io.fetchrequest.valid = isValid
+  io.fetchrequest.addr  := pc
+  io.fetchrequest.valid := isValid
   /*
   switch(state) {
     is(boot) {
