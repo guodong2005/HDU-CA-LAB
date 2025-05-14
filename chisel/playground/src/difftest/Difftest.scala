d8c78dd9e573a130f10549926800e6621d2db9a2
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-05-14 16:15:47 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.20-2-MANJARO #1 SMP PREEMPT_DYNAMIC Mon, 24 Mar 2025 06:33:36 +0000 x86_64 GNU/Linux  16:15:47 up  1:29,  1 user,  load average: 2.62, 1.55, 1.58

--- chisel/playground/src/difftest/Difftest.scala
+++ chisel/playground/src/difftest/Difftest.scala
@@ -10,18 +10,24 @@ class Diff extends Module {
     val debug   = Flipped(new DEBUG())
     val info    = Input(new Info())
     val regs_in = Input(Vec(AREG_NUM, UInt(XLEN.W)))
-    // Outputs :
+    // Outputs:
     val diffout = Output(new DiffOut())
   })
-  // if I want to change store/load event, I will decode here.
-  // because this module wont get boarded so no need to worry for the resource
-  io                           := DontCare
-  io.diffout                   := 0.U.asTypeOf(new DiffOut())
-  io.diffout.instrCommit.instr := io.info.instr
-  io.diffout.instrCommit.valid := io.debug.commit
-  io.diffout.instrCommit.pc    := io.debug.pc
-  io.diffout.instrCommit.wdest := io.debug.rf_wnum
-  io.diffout.instrCommit.wdata := io.debug.rf_wdata
-  io.diffout.gRegState         := io.regs_in
 
+  // Register to store previous diffout state
+  val diffout_reg = RegInit(0.U.asTypeOf(new DiffOut()))
+
+  when(io.debug.commit === true.B) {
+    // Update diffout_reg when commit is valid
+    diffout_reg.instrCommit.instr := io.info.instr
+    diffout_reg.instrCommit.valid := io.debug.commit
+    diffout_reg.instrCommit.pc    := io.debug.pc
+    diffout_reg.instrCommit.wdest := io.debug.rf_wnum
+    diffout_reg.instrCommit.wdata := io.debug.rf_wdata
+    diffout_reg.gRegState         := io.regs_in
+  }
+
+  // Set output based on stored state
+  io.diffout                   := diffout_reg
+  io.diffout.instrCommit.valid := Mux(io.debug.commit === true.B, diffout_reg.instrCommit.valid, 0.U)
 }
