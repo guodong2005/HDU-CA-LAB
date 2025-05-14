992b0061ab00b54424e40301247a5249e8519fbf
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-05-14 16:39:25 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.20-2-MANJARO #1 SMP PREEMPT_DYNAMIC Mon, 24 Mar 2025 06:33:36 +0000 x86_64 GNU/Linux  16:39:25 up  1:53,  1 user,  load average: 2.05, 2.07, 1.82

--- chisel/playground/src/difftest/Difftest.scala
+++ chisel/playground/src/difftest/Difftest.scala
@@ -10,27 +10,31 @@ class Diff extends Module {
     val debug   = Flipped(new DEBUG())
     val info    = Input(new Info())
     val regs_in = Input(Vec(AREG_NUM, UInt(XLEN.W)))
+    // Outputs:
     val diffout = Output(new DiffOut())
   })
 
   // Register to store previous diffout state
-  val diffout_reg = RegInit(0.U.asTypeOf(chiselTypeOf(io.diffout)))
+  val diffout_reg = RegInit(0.U.asTypeOf(new DiffOut()))
 
-  // When commit is true, update diffout with new values from debug/info
-  io.diffout := DontCare
   when(io.debug.commit =/= false.B) {
-    io.diffout.instrCommit.instr := io.info.instr
-    io.diffout.instrCommit.valid := io.debug.commit
-    io.diffout.instrCommit.pc    := io.debug.pc
-    io.diffout.instrCommit.wdest := io.debug.rf_wnum
-    io.diffout.instrCommit.wdata := io.debug.rf_wdata
-    io.diffout.gRegState         := io.regs_in
+    // Update diffout_reg when commit is valid
+    diffout_reg.instrCommit.instr := io.info.instr
+    diffout_reg.instrCommit.valid := io.debug.commit
+    diffout_reg.instrCommit.pc    := io.debug.pc
+    diffout_reg.instrCommit.wdest := io.debug.rf_wnum
+    diffout_reg.instrCommit.wdata := io.debug.rf_wdata
+    diffout_reg.gRegState         := io.regs_in
+    io.diffout.instrCommit.instr  := io.info.instr
+    io.diffout.instrCommit.valid  := io.debug.commit
+    io.diffout.instrCommit.pc     := io.debug.pc
+    io.diffout.instrCommit.wdest  := io.debug.rf_wnum
+    io.diffout.instrCommit.wdata  := io.debug.rf_wdata
+    io.diffout.gRegState          := io.regs_in
   }.otherwise {
-    // Otherwise, retain previous state from register
-    io.diffout                   := diffout_reg
-    io.diffout.instrCommit.valid := 0.U // Explicitly set valid to false
+    io.diffout := diffout_reg
   }
 
-  // Store the current `diffout` state into the register for future cycles
-  diffout_reg := io.diffout
+  // Set output based on stored state
+  io.diffout.instrCommit.valid := Mux(io.debug.commit =/= false.B, io.debug.commit, 0.U)
 }
