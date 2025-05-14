3bf38882bb387bdc42da181a0266c2710481de34
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-05-14 17:13:40 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.20-2-MANJARO #1 SMP PREEMPT_DYNAMIC Mon, 24 Mar 2025 06:33:36 +0000 x86_64 GNU/Linux  17:13:40 up  2:27,  1 user,  load average: 2.96, 2.20, 1.91

---
+++ chisel/playground/src/difftest/difftest.scala
@@ -0,0 +1,27 @@
+package cpu.pipeline
+
+import chisel3._
+import chisel3.util._
+import cpu.defines._
+import cpu.defines.Const._
+
+class Diff extends Module {
+  val io = IO(new Bundle {
+    val debug   = Flipped(new DEBUG())
+    val info    = Input(new Info())
+    val regs_in = Input(Vec(AREG_NUM, UInt(XLEN.W)))
+    // Outputs:
+    val diffout = Output(new DiffOut())
+  })
+
+  // Register to store previous diffout state
+  val diffout_reg = RegInit(0.U.asTypeOf(new DiffOut()))
+
+  io.diffout                   := DontCare
+  io.diffout.instrCommit.instr := io.info.instr
+  io.diffout.instrCommit.valid := io.debug.commit
+  io.diffout.instrCommit.pc    := io.debug.pc
+  io.diffout.instrCommit.wdest := io.debug.rf_wnum
+  io.diffout.instrCommit.wdata := io.debug.rf_wdata
+  io.diffout.gRegState         := io.regs_in
+}
