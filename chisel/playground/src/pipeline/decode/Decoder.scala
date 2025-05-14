068c4b454398b70454114e2f4b9b49ad55262d41
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-05-14 21:49:07 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.20-2-MANJARO #1 SMP PREEMPT_DYNAMIC Mon, 24 Mar 2025 06:33:36 +0000 x86_64 GNU/Linux  21:49:07 up  7:03,  1 user,  load average: 2.86, 2.68, 2.64

--- chisel/playground/src/pipeline/decode/Decoder.scala
+++ chisel/playground/src/pipeline/decode/Decoder.scala
@@ -16,10 +16,9 @@ class Decoder extends Module with HasInstrType {
       val info = new Info()
     })
   })
-  val inst = io.in.inst
   val instrType :: fuType :: fuOpType :: Nil =
-    ListLookup(inst, Instructions.DecodeDefault, Instructions.DecodeTable)
-  inst        := Mux(instrType === InstrN, Instructions.NOP, inst)
+    ListLookup(io.in.inst, Instructions.DecodeDefault, Instructions.DecodeTable)
+  val inst = Mux(instrType === InstrN, Instructions.NOP, io.in.inst)
   io.out.info := DontCare
 
   io.out.info.valid := false.B
