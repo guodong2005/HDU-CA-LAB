1bbb1073b861b086f8d3effff3478ff262d3dcdc
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-05-19 09:16:11 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.20-2-MANJARO #1 SMP PREEMPT_DYNAMIC Mon, 24 Mar 2025 06:33:36 +0000 x86_64 GNU/Linux  09:16:11 up  1:10,  1 user,  load average: 4.04, 3.19, 2.94

--- chisel/playground/src/pipeline/fetch/FetchUnit.scala
+++ chisel/playground/src/pipeline/fetch/FetchUnit.scala
@@ -65,10 +65,10 @@ class FetchUnit extends Module {
       isValid := true.B
     }
   }.otherwise {
-    pc := Mux(stall | icacheStall, pc, nxtpc)
+    pc := Mux(stall | io.icacheStall, pc, nxtpc)
   }
   io.fetchrequest.addr  := pc
-  io.fetchrequest.valid := !stall & !icacheStall // stall 不要发送请求
+  io.fetchrequest.valid := !stall & !io.icacheStall // stall 不要发送请求
 
 }
 
