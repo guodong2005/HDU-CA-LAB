fa682d0ed0adf8ace311ad7fdf638b4a5fbde0d3
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-05-20 08:22:52 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.20-2-MANJARO #1 SMP PREEMPT_DYNAMIC Mon, 24 Mar 2025 06:33:36 +0000 x86_64 GNU/Linux  08:22:52 up 7 min,  1 user,  load average: 2.79, 2.12, 1.11

--- chisel/playground/src/pipeline/control/AxiBridge.scala
+++ chisel/playground/src/pipeline/control/AxiBridge.scala
@@ -26,7 +26,9 @@ class Axibridge extends Module {
     val icacheInput = Flipped(new AXI())
   })
 
-  io.axi := DontCare
+  io.axi         := DontCare
+  io.dcacheInput := DontCare
+  io.icacheInput := DontCare
   // ------------------------------------------------------------
   // Latch incoming AR requests from caches.
   // ------------------------------------------------------------
