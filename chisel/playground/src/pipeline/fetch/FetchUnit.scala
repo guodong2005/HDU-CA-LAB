6651503df3eed61cc190322e1b774970af696afb
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-05-20 08:20:56 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.20-2-MANJARO #1 SMP PREEMPT_DYNAMIC Mon, 24 Mar 2025 06:33:36 +0000 x86_64 GNU/Linux  08:20:56 up 5 min,  1 user,  load average: 2.66, 1.98, 0.95

--- chisel/playground/src/pipeline/fetch/FetchUnit.scala
+++ chisel/playground/src/pipeline/fetch/FetchUnit.scala
@@ -75,7 +75,7 @@ class FetchUnit extends Module {
   val decodeReady = io.signal.fetchUnitSignal.allow_to_go
   val stall       = !decodeReady || ifid_reg.valid
 
-  -------------------------------------------------------------------------
+  // -------------------------------------------------------------------------
   // Drive the fetch request (AXI AR channel).
   // A new fetch request is issued (with the current pc) only when:
   //   - The fetch unit is in the idle state (sIdle),
