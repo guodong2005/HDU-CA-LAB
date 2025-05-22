6dd2bfad6a0fd8f2815e99ca7a621890e13ac64c
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-05-22 16:46:26 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.20-2-MANJARO #1 SMP PREEMPT_DYNAMIC Mon, 24 Mar 2025 06:33:36 +0000 x86_64 GNU/Linux  16:46:26 up 30 min,  1 user,  load average: 1.32, 1.46, 1.50

--- chisel/playground/src/pipeline/execute/ExecuteUnit.scala
+++ chisel/playground/src/pipeline/execute/ExecuteUnit.scala
@@ -39,6 +39,7 @@ class ExecuteUnit extends Module {
   val srcinfo = Mux(io.executeStage.data.info.valid, io.executeStage.data.src_info, srcInfoReg)
   val info    = Mux(io.executeStage.data.info.valid, io.executeStage.data.info, infoReg)
   val pc      = Mux(io.executeStage.data.info.valid, io.executeStage.data.pc, pcReg)
+  printf(p"Hexadecimal PC: 0x${Hexadecimal(io.executeStage.data.pc)}\n")
 
   fu.io.dcache        <> io.dcache
   fu.io.data.pc       := pc
