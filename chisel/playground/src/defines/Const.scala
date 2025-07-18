0f2ac22a796cf03c2deea0c0bd1ff568685d9e26
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-07-18 19:40:42 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.28-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 09 May 2025 10:53:27 +0000 x86_64 GNU/Linux  19:40:42 up  1:53,  1 user,  load average: 2.74, 2.71, 2.67

--- chisel/playground/src/defines/Const.scala
+++ chisel/playground/src/defines/Const.scala
@@ -24,6 +24,26 @@ trait Constants extends CoreParameter {
   // GPR RegFile
   val AREG_NUM     = 32
   val REG_ADDR_WID = 5
+  // do not change Icache const
+  val FETCH_WIDTH         = 8
+  val ICACHE_DEPTH        = 128
+  val ICACHE_OFFSET_WIDTH = log2Up(FETCH_WIDTH * 4)
+  val ICACHE_INST_WIDTH   = log2Up(FETCH_WIDTH)
+  val ICACHE_INDEX_WIDTH  = log2Up(ICACHE_DEPTH)
+  val ICACHE_TAG_WIDTH    = 32 - ICACHE_INDEX_WIDTH - ICACHE_OFFSET_WIDTH
+
+  val FETCH_BUFFER_DEPTH = 17
+
+  val COMMIT_WIDTH       = 2
+  val ISSUE_WIDTH        = 2
+  val WRITE_BUFFER_DEPTH = 4
+  val LOAD_QUEUE_DEPTH   = 8
+  val DISPATCH_WIDTH     = 5
+  val SRAM_DELAY         = 1
+
+  val ROB_DEPTH         = 16
+  val ROB_IDX_WIDTH     = log2Up(ROB_DEPTH)
+  val UART_BUFFER_DEPTH = 8
 }
 
 trait SRAMConst extends Constants {
