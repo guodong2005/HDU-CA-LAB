623fd1a26495063d00e55d65a487f7bb0041573b
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-07-18 19:18:59 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.28-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 09 May 2025 10:53:27 +0000 x86_64 GNU/Linux  19:18:59 up  1:31,  1 user,  load average: 2.72, 2.84, 2.85

--- chisel/playground/src/pipeline/fetch/FetchUnit.scala
+++ chisel/playground/src/pipeline/fetch/FetchUnit.scala
@@ -47,6 +47,9 @@ class FetchUnit extends Module {
 
   switch(state) {
     is(sIdle) {
+      when(io.branch) {
+        pc := io.target
+      }
       when(canStart === false.B) {
         state := sIdle
       }.elsewhen(io.fetchrequest.valid && io.fetchrequest.ready) {
@@ -55,7 +58,8 @@ class FetchUnit extends Module {
       }
     }
     is(sWait) {
-      val answerMatches = (io.fetchanswer.pc === reqPC)
+      val answerMatches = true.B
+      // val answerMatches = (io.fetchanswer.pc === reqPC)
       when(io.branch) {
         pc    := io.target
         state := sIdle
