4fd4200af48b660f0b99af05b69ba6272bc428a4
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-07-27 14:06:31 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.28-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 09 May 2025 10:53:27 +0000 x86_64 GNU/Linux  14:06:31 up 44 min,  1 user,  load average: 1.35, 1.69, 1.70

--- chisel/playground/src/pipeline/control/ControlUnit.scala
+++ chisel/playground/src/pipeline/control/ControlUnit.scala
@@ -45,8 +45,9 @@ class ControlUnit extends Module {
   // Generate control signals using modular assignment
   io.signals.fetchUnitSignal.allow_to_go := (!pipeline_stall) & (io.executeUnitReady)
 
-  io.signals.decodeUnitSignal.allow_to_go  := (!pipeline_stall) & (io.executeUnitReady)
-  io.signals.executeUnitSignal.allow_to_go := io.executeUnitReady
+  io.signals.decodeUnitSignal.allow_to_go := (!pipeline_stall) & (io.executeUnitReady)
+  //io.signals.executeUnitSignal.allow_to_go := io.executeUnitReady
+  io.signals.executeUnitSignal.allow_to_go := true.B
   // dontTouch(io.signals)
 // icache stall 只会影响 fetchUnit
 
