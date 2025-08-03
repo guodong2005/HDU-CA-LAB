14c977cb46d48097d88032491a9c5ae5882d805d
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-08-03 15:23:11 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.28-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 09 May 2025 10:53:27 +0000 x86_64 GNU/Linux  15:23:11 up  1:34,  1 user,  load average: 2.96, 2.06, 1.80

--- chisel/playground/src/defines/Const.scala
+++ chisel/playground/src/defines/Const.scala
@@ -34,15 +34,11 @@ trait Constants extends CoreParameter {
 
   val FETCH_BUFFER_DEPTH = 17
 
-  val COMMIT_WIDTH       = 2
-  val ISSUE_WIDTH        = 2
   val WRITE_BUFFER_DEPTH = 4
   val LOAD_QUEUE_DEPTH   = 8
   val DISPATCH_WIDTH     = 5
-  val SRAM_DELAY         = 1
+  val SRAM_DELAY         = 3
 
-  val ROB_DEPTH         = 16
-  val ROB_IDX_WIDTH     = log2Up(ROB_DEPTH)
   val UART_BUFFER_DEPTH = 8
 }
 
