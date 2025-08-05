08080b6b6fa1b86f47e93bc268f3678aef5bb202
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-08-05 15:45:54 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.28-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 09 May 2025 10:53:27 +0000 x86_64 GNU/Linux  15:45:54 up  5:12,  1 user,  load average: 2.99, 2.58, 2.86

--- chisel/playground/src/pipeline/control/ControlUnit.scala
+++ chisel/playground/src/pipeline/control/ControlUnit.scala
@@ -226,7 +226,7 @@ class ControlUnit extends Module {
   io.signals.executeUnitSignal.allow_to_go := true.B
   io.signals.memoryUnitSignal.allow_to_go  := true.B
 
-  io.signals.fetchUnitSignal.do_flush   := io.branch
+  io.signals.fetchUnitSignal.do_flush   := false.B
   io.signals.decodeUnitSignal.do_flush  := false.B
   io.signals.executeUnitSignal.do_flush := false.B
   io.signals.memoryUnitSignal.do_flush  := false.B
