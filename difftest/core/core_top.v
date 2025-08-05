1fc76a17d13b24a26c1ab4acc2200fa808e38223
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-08-05 17:29:57 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.28-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 09 May 2025 10:53:27 +0000 x86_64 GNU/Linux  17:29:57 up  6:56,  1 user,  load average: 2.47, 3.01, 3.19

--- difftest/core/core_top.v
+++ difftest/core/core_top.v
@@ -2958,16 +2958,35 @@ module Alu(
   output        io_valid
 );
 
-  wire [62:0] _results_5_T_1 =
+  wire [62:0] _io_result_T_20 =
     {31'h0, io_src_info_src1_data} << io_src_info_src2_data[4:0];
   assign io_result =
-    (io_info_op == 5'h0 ? io_src_info_src1_data + io_src_info_src2_data : 32'h0)
-    | (io_info_op == 5'h1 ? io_src_info_src1_data - io_src_info_src2_data : 32'h0)
-    | (io_info_op == 5'h2 ? io_src_info_src1_data & io_src_info_src2_data : 32'h0)
-    | (io_info_op == 5'h3 ? io_src_info_src1_data | io_src_info_src2_data : 32'h0)
-    | (io_info_op == 5'h4 ? io_src_info_src1_data ^ io_src_info_src2_data : 32'h0)
-    | (io_info_op == 5'h5 ? _results_5_T_1[31:0] : 32'h0)
-    | (io_info_op == 5'h6 ? io_src_info_src1_data >> io_src_info_src2_data[4:0] : 32'h0);
+    io_info_op == 5'h0
+      ? io_src_info_src1_data + io_src_info_src2_data
+      : io_info_op == 5'h1
+          ? io_src_info_src1_data - io_src_info_src2_data
+          : io_info_op == 5'h8
+              ? io_src_info_src1_data & io_src_info_src2_data
+              : io_info_op == 5'h7
+                  ? io_src_info_src1_data | io_src_info_src2_data
+                  : io_info_op == 5'h4
+                      ? io_src_info_src1_data ^ io_src_info_src2_data
+                      : io_info_op == 5'hC
+                          ? ~(io_src_info_src1_data | io_src_info_src2_data)
+                          : io_info_op == 5'h2
+                              ? {31'h0,
+                                 $signed(io_src_info_src1_data) < $signed(io_src_info_src2_data)}
+                              : io_info_op == 5'h3
+                                  ? {31'h0, io_src_info_src1_data < io_src_info_src2_data}
+                                  : io_info_op == 5'h9
+                                      ? _io_result_T_20[31:0]
+                                      : io_info_op == 5'h5
+                                          ? io_src_info_src1_data
+                                            >> io_src_info_src2_data[4:0]
+                                          : io_info_op == 5'h6
+                                              ? $signed($signed(io_src_info_src1_data)
+                                                        >>> io_src_info_src2_data[4:0])
+                                              : 32'h0;
   assign io_valid = io_info_valid;
 endmodule
 
