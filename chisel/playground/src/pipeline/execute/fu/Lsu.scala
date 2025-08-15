166bee8344f16b52e8636f57c390467239259343
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-08-15 22:28:41 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.28-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 09 May 2025 10:53:27 +0000 x86_64 GNU/Linux  22:28:41 up  1:18,  1 user,  load average: 3.35, 2.98, 2.18

--- chisel/playground/src/pipeline/execute/fu/Lsu.scala
+++ chisel/playground/src/pipeline/execute/fu/Lsu.scala
@@ -29,6 +29,7 @@ class Lsu extends Module {
   val writeBuffer = Module(new WriteBuffer(depth = 4))
   writeBuffer.io.flush        := false.B
   writeBuffer.io.bypassEnable := true.B
+  dontTouch(writeBuffer.io)
 
   val isStore       = isLsu && LSUOpType.isStore(io.info.op)
   val isLoad        = isLsu && !isStore
