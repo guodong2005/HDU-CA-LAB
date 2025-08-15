5b9b7a9ad074af0186f57ffb8b2ac0f3a8887943
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-08-15 22:55:51 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.28-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 09 May 2025 10:53:27 +0000 x86_64 GNU/Linux  22:55:51 up  1:45,  1 user,  load average: 4.37, 3.50, 2.87

--- difftest/core/core_top.v
+++ difftest/core/core_top.v
@@ -2959,13 +2959,13 @@ module WriteBuffer(
   wire [3:0][2:0]  _GEN_3 =
     {{buffer_3_req_size}, {buffer_2_req_size}, {buffer_1_req_size}, {buffer_0_req_size}};
   wire             hits_0 =
-    valids_0 & io_bypassEnable & buffer_0_req_write & buffer_0_req_addr == io_bypassAddr;
+    io_bypassEnable & buffer_0_req_write & buffer_0_req_addr == io_bypassAddr;
   wire             hits_1 =
-    valids_1 & io_bypassEnable & buffer_1_req_write & buffer_1_req_addr == io_bypassAddr;
+    io_bypassEnable & buffer_1_req_write & buffer_1_req_addr == io_bypassAddr;
   wire             hits_2 =
-    valids_2 & io_bypassEnable & buffer_2_req_write & buffer_2_req_addr == io_bypassAddr;
+    io_bypassEnable & buffer_2_req_write & buffer_2_req_addr == io_bypassAddr;
   wire             hits_3 =
-    valids_3 & io_bypassEnable & buffer_3_req_write & buffer_3_req_addr == io_bypassAddr;
+    io_bypassEnable & buffer_3_req_write & buffer_3_req_addr == io_bypassAddr;
   always @(posedge clock) begin
     if (reset) begin
       buffer_0_req_addr <= 32'h0;
