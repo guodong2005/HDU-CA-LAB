0d8656eaea8d50f16d482295ce1aeccc42ce8356
tracer-rvlab <tracer@kairos.hdu.edu.cn>
2025-08-17 01:29:15 +0800

    > generate verilog 23150919 WangJiadong Linux guodong-manjaro 6.12.28-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 09 May 2025 10:53:27 +0000 x86_64 GNU/Linux  01:29:15 up 12:08,  1 user,  load average: 4.17, 3.87, 3.73

--- chisel/playground/src/pipeline/execute/ExecuteUnit.scala
+++ chisel/playground/src/pipeline/execute/ExecuteUnit.scala
@@ -27,10 +27,11 @@ class ExecuteUnit extends Module {
   fu.io.data.info     := io.executeStage.data.info
   fu.io.data.src_info := io.executeStage.data.src_info
 
-  io.result := fu.io.data.rd_info.wdata
-  io.ready  := fu.io.data.ready
-  io.branch := fu.io.data.branch
-  io.target := fu.io.data.target
+  io.result      := fu.io.data.rd_info.wdata
+  io.bpufeedback := fu.io.data.bpufeedback
+  io.ready       := fu.io.data.ready
+  io.branch      := fu.io.data.branch
+  io.target      := fu.io.data.target
 
   // 直接输出到WriteBack阶段，不再经过Memory阶段
   io.writeBackStage.data.pc           := io.executeStage.data.pc
